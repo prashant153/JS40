23e810310bf58e28cba8230e84402e6a3f4fea35
prashant kumar <prashant153@gmail.com>
2021-04-24 23:16:01 +0530

    code changes include: - converting function component to class based component for async operations - using state - 2 ways of initializing state object - conditional rendering of data - using componentDidMount for data loading

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hi there!!</div>
-};
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     position => console.log(position),
+//     err => console.log(err)
+//   );
+//   return <div>Hi there!!</div>
+// };
+
+// Converted the above function component to class component to handle async calls:
+// Step1: Convert to Class component
+// Step2: Make use of State
+
+class App extends React.Component{
+  // 1st way for state initialization
+  // constructor(props){
+  //   super(props);
+  //   // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT OF STATE OBJECT PROPERTIES
+  //   this.state = { lat: null, errorMessage: ''};
+  // }
+
+  // 2nd way of state initialization (syntactic sugar)
+
+  state = {lat: null, errorMessage: ''};
+
+  componentDidMount(){  
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // WE ALWAYS CALL  this.setState()
+        this.setState({lat: position.coords.latitude});
+        // we never ever do this assignment
+        // this.state.lat = position.coords.latitude
+      },
+      err =>this.setState({errorMessage: err.message})
+    );
+  }
+
+  render(){
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latidude: {this.state.lat}</div> 
+    }
+    return <div>Loading!!</div>
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'))
