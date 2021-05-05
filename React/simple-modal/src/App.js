b60612625168e8336c5ea20e655cad172153dc1f
prashant kumar <prashant153@gmail.com>
2021-05-05 16:07:34 +0530

    Added link to see toggle instead of onClose as separate function

--- React/simple-modal/src/App.js
+++ React/simple-modal/src/App.js
@@ -14,9 +14,11 @@ class App extends React.Component {
     this.setState({showmodal: false});
   }
 
+  // TODO: Refer the below code for toggle instead of onClose
+  // https://blog.bitsrc.io/build-a-simple-modal-component-with-react-16decdc111a6
   render(){
     return (<div>
-      <button className="show-modal"  onClick={this.onButtonClick}>Show modal</button>
+      <button className="show-modal"  onClick={this.onButtonClick}>Show modal</button>      
       <Modal onClose={this.onClose} showmodal={this.state.showmodal}/>
     </div>   
   )
