3f07cbc0e56dae9aa979656df1c70124d3d3325f
prashant kumar <prashant153@gmail.com>
2021-05-06 13:58:00 +0530

    Code for vanilla js color flipper

---
+++ Vanilla/ColorFlipper/hex.js
@@ -0,0 +1,18 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn =  document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    const randomHexColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomHexColor;
+    color.textContent = randomHexColor;    
+});
+
+const getRandomHexColor = () => {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {   
+        hexColor += hex[Math.floor(Math.random()*hex.length)];
+    }
+    return hexColor;
+};
